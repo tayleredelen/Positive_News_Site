42bba9e456f2cbd79caa26c1463ed523989f4736
tayleredelen <taylerledelen@gmail.com>
2023-10-15 11:29:01 -0500

    Day 27 -- URL requests

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import requests
+
+api_key = "c0bb248e83674588a3b909f3118faf6d"
+url = ("https://newsapi.org/v2/everything?q=tesla&"
+       "from=2023-09-15&sortBy=publishedAt&apiKey="
+       "c0bb248e83674588a3b909f3118faf6d")
+
+request = requests.get(url)
+content = request.text
+print(content)
