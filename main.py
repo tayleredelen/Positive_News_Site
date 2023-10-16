d0db275f60de87ef9979d6ef9af16432096fecff
tayleredelen <taylerledelen@gmail.com>
2023-10-16 12:33:32 -0500

    Day 27 -- Access news titles and description. Added README.md and .gitignore files.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests
 
 api_key = "c0bb248e83674588a3b909f3118faf6d"
-url = ("https://newsapi.org/v2/everything?q=tesla&"
-       "from=2023-09-15&sortBy=publishedAt&apiKey="
-       "c0bb248e83674588a3b909f3118faf6d")
+url = ("https://newsapi.org/v2/everything?q=tesla&from=2023-09-16&sortBy=publishedAt&apiKey"
+       "=c0bb248e83674588a3b909f3118faf6d")
 
+# Make request
 request = requests.get(url)
-content = request.text
-print(content)
+
+# Get a dictionary with data
+content = request.json()
+
+# Access the article titles and descriptions
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
