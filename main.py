75ac29a830e035bea83881abde2fd3733e02e8c0
tayleredelen <taylerledelen@gmail.com>
2023-10-18 10:45:00 -0500

    Day 28 -- Add links

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
 api_key = "c0bb248e83674588a3b909f3118faf6d"
-url = ("https://newsapi.org/v2/everything?q=tesla&from=2023-09-18&sortBy=publishedAt&apiKey=" \
-       "c0bb248e83674588a3b909f3118faf6d")
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&from=2023-09-18&sortBy=publishedAt&apiKey="
+       "c0bb248e83674588a3b909f3118faf6d&"
+       "language=en")
 
 # Make request
 request = requests.get(url)
@@ -15,7 +18,10 @@ content = request.json()
 body = ""
 for article in content["articles"]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + str(article["description"]) + 2 * "\n"
+        body = ("Subject: Today's news" + "\n" +
+                body + article["title"] + "\n"
+                + str(article["description"]) +
+                "\n" + article["url"] + 2 * "\n")
 
 body = body.encode("utf-8")
 send_email(message=body)
